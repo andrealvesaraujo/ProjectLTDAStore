2370a7d04a8e4b7ee7d4883afe87377e8705aa00
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-11 22:53:11 -0300

    feature: import fonts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react"
+import './fonts/Poppins-Regular.ttf';
+import './fonts/Poppins-Medium.ttf';
+import './fonts/Poppins-Black.ttf';
+import './fonts/Poppins-SemiBold.ttf';
+import './fonts/Poppins-Bold.ttf';
 import './App.scss';
 
 function App() {
