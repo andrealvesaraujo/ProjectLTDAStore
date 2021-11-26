0cdeb63237962c9001b39d5ba11cff03a2e8c398
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-26 17:21:27 -0300

    Add Fade component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { faFacebookF, faYoutube, faInstagram , faTwitter} from "@fortawesome/fre
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import '../node_modules/react-modal-video/scss/modal-video.scss';
 import ModalVideo from 'react-modal-video'
+import Fade from 'react-reveal/Fade';
 export default class App extends React.Component {
 
   constructor(props) {
@@ -32,32 +33,35 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="content">
-        <header>
-          <img src={Logo} alt="Logo" />
-          <nav>
-            <a href="https://www.facebook.com/Nolojinhaltda" alt="Facebook" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faFacebookF} /></a>
-            <a href="https://www.instagram.com/nolojinhaltda/" alt="Instagram" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faInstagram} /></a>
-            <a href="/" alt="Twitter" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faTwitter} /></a>
-            <a href="https://www.youtube.com/channel/UCm5cgfZlVsyjE4SeUnAqF4w" alt="Youtube" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faYoutube} /></a>
-          </nav>
-        </header>
-        <div className="container" >
-          <main>
-            <h2>Melhor Canal</h2>
-            <h1>No Lojinha LTDA</h1>
-            <p>Falando sobre tudo que a gente curte, sem conhecimento técnico <br />
-              e muito pautado no achismo que nossas vidas são... <br />
-              Bora cara que eu tenho que acordar cedo amanha.
-            </p>
-            <div>
-              <button className="container-primary" onClick={()=> this.handleWatchNow(true)}><FontAwesomeIcon className="icon-button" icon={faPlay} /> Marvel vs DC</button>
-              <button className="container-outline" onClick={()=> this.handleWatchTrailer(true)}><FontAwesomeIcon className="icon-button" icon={faPlay} /> Vilões da Disney</button>
-              <ModalVideo channel='youtube' autoplay isOpen={this.state.isWatchNow} videoId="-C-TkiHR59U" onClose={() => this.handleWatchNow(false)} />
-              <ModalVideo channel='youtube' autoplay isOpen={this.state.isWatchTrailer} videoId="Ccb_zZ430Eo" onClose={() => this.handleWatchTrailer(false)} />
+          <Fade>
+            <header>
+              <img src={Logo} alt="Logo" />
+              <nav>
+                  <a href="https://www.facebook.com/Nolojinhaltda" alt="Facebook" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faFacebookF} /></a>
+                <a href="https://www.instagram.com/nolojinhaltda/" alt="Instagram" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faInstagram} /></a>
+                <a href="/" alt="Twitter" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faTwitter} /></a>
+                <a href="https://www.youtube.com/channel/UCm5cgfZlVsyjE4SeUnAqF4w" alt="Youtube" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon className="icon" icon={faYoutube} /></a>
+              </nav>
+            </header>
+            <div className="container" >
+              <main>
+                <h2>Melhor Canal Nerd !!!</h2>
+                <h1>No Lojinha LTDA</h1>
+                <p>Falando sobre tudo que a gente curte, sem conhecimento técnico <br />
+                  e muito pautado no achismo que nossas vidas são... <br />
+                  Bora cara que eu tenho que acordar cedo amanha.
+                </p>
+                <div>
+                  <button className="container-primary" onClick={()=> this.handleWatchNow(true)}><FontAwesomeIcon className="icon-button" icon={faPlay} /> Marvel vs DC</button>
+                  <button className="container-outline" onClick={()=> this.handleWatchTrailer(true)}><FontAwesomeIcon className="icon-button" icon={faPlay} /> Vilões da Disney</button>
+                  <ModalVideo channel='youtube' autoplay isOpen={this.state.isWatchNow} videoId="-C-TkiHR59U" onClose={() => this.handleWatchNow(false)} />
+                  <ModalVideo channel='youtube' autoplay isOpen={this.state.isWatchTrailer} videoId="Ccb_zZ430Eo" onClose={() => this.handleWatchTrailer(false)} />
+                </div>
+              </main>
             </div>
-          </main>
+          </Fade>
         </div>
-      </div>
+      
     );
   }
 }
